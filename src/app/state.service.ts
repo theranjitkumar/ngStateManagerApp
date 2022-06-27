f9c415c5330e37eebb4246de330a27218e82d1a6
ranjit.kumar <ranjit.kumar@mobileprogramming.com>
2022-06-27 23:00:21 +0530

    state.service file updated

--- src/app/state.service.ts
+++ src/app/state.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject, Observable } from 'rxjs';
+class LocalState {
+  public counter: number;
+  constructor() {
+    this.counter = 0;
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
+  public localStateData: LocalState = new LocalState();
+  public localState = new BehaviorSubject<any>(this.localStateData); // Handle Local storage
 
-  constructor() { }
+  constructor() {
+    if (localStorage.hasOwnProperty('localState')) {
+      let localStorageData: any = localStorage.getItem('localState');
+      this.localState.next(JSON.parse(localStorageData));
+    }
+    this.localState.subscribe((updatedData: any) => {
+      this.localStateData = updatedData;
+      localStorage.setItem('localState', JSON.stringify(this.localStateData));
+    })
 
+  }
+
+  public updatedData(data: any) {
+    this.localState.next(data);
+    sessionStorage.setItem('localState', JSON.stringify(data))
+  }
 
 }
